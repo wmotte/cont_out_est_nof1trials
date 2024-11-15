a588fc3903f43b17a932b319e5da21907445827a
FBartos <f.bartos96@gmail.com>
2024-11-15 17:43:35 +0100

    additional analyses

--- 10__BF__headache.R
+++ 10__BF__headache.R
@@ -25,13 +25,13 @@ number_ticks <- function( n ) { function( limits ) pretty( limits, n ) }
 get_data <- function()
 {
     # read first tab sheet
-    infile <- 'datasets/SMS.csv'   
+    infile <- 'datasets/SMS.csv'
     df <- read.csv( infile, stringsAsFactors = TRUE, row.names = 1 )
 
     df[ df$group == 'injection', 'group' ] <- 'baseline'
 
     rownames( df ) <- NULL
-    
+
     return( df )
 }
 
@@ -41,34 +41,67 @@ get_data <- function()
 ##
 get_bayes_factor <- function( data )
 {
-    # truncate gamma and normal priors for the intercept and slope within reasonable 
-    # severity ranges (0-1 for intercept and -1 to +1 for group) 
+    # truncate gamma and normal priors for the intercept and slope within reasonable
+    # severity ranges (0-1 for intercept and -1 to +1 for group)
     priors <- c(
         prior( gamma( 2, 3 ), class = "Intercept", lb = 0, ub = 1 ),
-        prior( normal( 0, 5 ), class = "b", lb = -1, ub = 1 )  
+        prior( normal( 0, 5 ), class = "b", lb = -1, ub = 1 )
     )
-    
+
     # fit Bayesian model (treatment)
     # refresh = 0 turns off iteration info print
     bmodel_H1 <- brms::brm( delta_severity ~ group, data = data,
                             iter = 25000, warmup = 3000,
                             prior = priors,
                             save_pars = save_pars( all = TRUE ), refresh = 0 )
-    
+
     # update Bayesian model [H0], with intercept only
-    bmodel_H0 <- update( bmodel_H1, formula = delta_severity ~ 1, refresh = 0 ) 
-    
+    bmodel_H0 <- update( bmodel_H1, formula = delta_severity ~ 1, refresh = 0 )
+
     # Bayes Factor, through bridge sampling
     BF_brms_bridge <- brms::bayes_factor( bmodel_H1, bmodel_H0, log = TRUE )
-    
+
     print( BF_brms_bridge )
-    
+
     # output, last row of data
     output <- data[ nrow( data ), ]
     output$log_bf <- BF_brms_bridge$bf
-    
+
     return( output )
-    
+
+}
+
+get_bayes_factor2 <- function( data )
+{
+  # truncate gamma and normal priors for the intercept and slope within reasonable
+  # severity ranges (0-1 for intercept and -1 to +1 for group)
+  priors <- c(
+    prior( gamma( 2, 3 ), class = "Intercept", lb = 0, ub = 1 ),
+    prior( normal( 0, 5 ), class = "b", lb = -1, ub = 1 )
+  )
+
+  # fit Bayesian model (treatment)
+  # refresh = 0 turns off iteration info print
+  data$group2 <- ifelse(data$group == "placebo", -0.5, 0.5)
+  bmodel_H1 <- brms::brm( delta_severity ~ group2, data = data,
+                          iter = 25000, warmup = 3000,
+                          prior = priors,
+                          save_pars = save_pars( all = TRUE ), refresh = 0 )
+
+  # update Bayesian model [H0], with intercept only
+  bmodel_H0 <- update( bmodel_H1, formula = delta_severity ~ 1, refresh = 0 )
+
+  # Bayes Factor, through bridge sampling
+  BF_brms_bridge <- brms::bayes_factor( bmodel_H1, bmodel_H0, log = TRUE )
+
+  print( BF_brms_bridge )
+
+  # output, last row of data
+  output <- data[ nrow( data ), ]
+  output$log_bf <- BF_brms_bridge$bf
+
+  return( output )
+
 }
 
 ################################################################################
@@ -93,12 +126,42 @@ data_day_2 <- df[ df$day <= 2, ]
 data_day_3 <- df[ df$day <= 3, ]
 data_day_4 <- df[ df$day <= 4, ]
 
-
 # get BFs
 bf1 <- get_bayes_factor( data_day_1 )
 bf2 <- get_bayes_factor( data_day_2 )
 bf3 <- get_bayes_factor( data_day_3 )
 bf4 <- get_bayes_factor( data_day_4 )
+
+# centered parameterization BFs (verifying that the results are equivalent)
+bf1b <- get_bayes_factor2( data_day_1 )
+bf2b <- get_bayes_factor2( data_day_2 )
+bf3b <- get_bayes_factor2( data_day_3 )
+bf4b <- get_bayes_factor2( data_day_4 )
+# this is OK
+
+# BIC Bayes factors
+(BICbf1 <- bayestestR::bic_to_bf(BIC(lm(delta_severity ~ group, data = data_day_1)), BIC(lm(delta_severity ~ 1, data = data_day_1)), log = FALSE))
+(BICbf2 <- bayestestR::bic_to_bf(BIC(lm(delta_severity ~ group, data = data_day_2)), BIC(lm(delta_severity ~ 1, data = data_day_2)), log = FALSE))
+(BICbf3 <- bayestestR::bic_to_bf(BIC(lm(delta_severity ~ group, data = data_day_3)), BIC(lm(delta_severity ~ 1, data = data_day_3)), log = FALSE))
+(BICbf4 <- bayestestR::bic_to_bf(BIC(lm(delta_severity ~ group, data = data_day_4)), BIC(lm(delta_severity ~ 1, data = data_day_4)), log = FALSE))
+# these show much more evidence in favor of the alternative hypothesis, this is worrying as an informed prior should generally provide more evidence
+# for the alternative hypothesis (if the alternative is true) than the unit information prior
+
+# another common default option would be JZS (which uses standardized effect sizes)
+summary(BAS::bas.lm(delta_severity ~ group, data = data_day_1, prior = "JZS"))
+summary(BAS::bas.lm(delta_severity ~ group, data = data_day_2, prior = "JZS"))
+summary(BAS::bas.lm(delta_severity ~ group, data = data_day_3, prior = "JZS"))
+summary(BAS::bas.lm(delta_severity ~ group, data = data_day_4, prior = "JZS"))
+# the JZS prior however does not provide that much evidence in favor of the alternative hypothesis either (I would've expected stronger support for the model 1)
+
+# similarly to the BayesFactor package
+BayesFactor::lmBF(delta_severity ~ group, data = data_day_1)
+BayesFactor::lmBF(delta_severity ~ group, data = data_day_2)
+BayesFactor::lmBF(delta_severity ~ group, data = data_day_3)
+BayesFactor::lmBF(delta_severity ~ group, data = data_day_4)
+
+# these information together might indicate that the selected prior distribution is not as bad
+# however, it is definitelly much less informative than it could be
 
 container1 <- rbind( bf1, bf2 )
 container2 <- rbind( bf3, bf4 )
--- 12__BF__hippocampal_stim.R
+++ 12__BF__hippocampal_stim.R
@@ -14,7 +14,7 @@ options( readr.show_progress = FALSE )
 # CUSTOM FUNCTIONS                                                             #
 ################################################################################
 
-### 
+###
 # Helper function plot.
 ##
 number_ticks <- function( n ) { function( limits ) pretty( limits, n ) }
@@ -25,12 +25,12 @@ number_ticks <- function( n ) { function( limits ) pretty( limits, n ) }
 get_data <- function()
 {
     # read first tab sheet
-    infile <- 'datasets/Tellez-Zenteno.csv'   
+    infile <- 'datasets/Tellez-Zenteno.csv'
     df <- read.csv( infile, stringsAsFactors = TRUE, row.names = 1 )
     df$time <- 1:nrow( df )
-    
+
     rownames( df ) <- NULL
-    
+
     return( df )
 }
 
@@ -39,33 +39,64 @@ get_data <- function()
 ##
 get_bayes_factor <- function( data )
 {
-    # truncate normal priors for the intercept and slope within reasonable seizure loads 
+    # truncate normal priors for the intercept and slope within reasonable seizure loads
     priors <- c(
         prior( normal( 0, 100 ), class = "Intercept", lb = -10, ub = 10 ),
-        prior( normal( 0, 100 ), class = "b", lb = -5, ub = 5 )  
+        prior( normal( 0, 100 ), class = "b", lb = -5, ub = 5 )
     )
-    
+
     # fit Bayesian Poisson model (treatment)
-    bmodel_H1 <- brms::brm( seizures ~ treatment, data = data, 
+    bmodel_H1 <- brms::brm( seizures ~ treatment, data = data,
                             iter = 25000, warmup = 3000,
-                            family = poisson(), 
+                            family = poisson(),
                             prior = priors,
                             save_pars = save_pars( all = TRUE ), refresh = 0 )
-    
+
     # update Bayesian Poisson model [H0], with intercept only
     bmodel_H0 <- update( bmodel_H1, formula = seizures ~ 1, refresh = 0 )
-    
+
     # Bayes Factor, through bridge sampling
     BF_brms_bridge <- brms::bayes_factor( bmodel_H1, bmodel_H0, log = TRUE )
-    
+
     print( BF_brms_bridge )
-    
+
     # output, last row of data
     output <- data[ nrow( data ), ]
     output$log_bf <- BF_brms_bridge$bf
-    
+
     return( output )
-    
+
+}
+
+get_bayes_factor2 <- function( data )
+{
+  # truncate normal priors for the intercept and slope within reasonable seizure loads
+  priors <- c(
+    prior( normal( 0, 100 ), class = "Intercept", lb = -10, ub = 10 ),
+    prior( normal( 0, log(2)/2 ), class = "b" )  # log(2)/2 ascertains that 95$ of the posterior probability is within the 1/2 to 2x rate reduction range
+  )
+
+  # fit Bayesian Poisson model (treatment)
+  bmodel_H1 <- brms::brm( seizures ~ treatment, data = data,
+                          iter = 25000, warmup = 3000,
+                          family = poisson(),
+                          prior = priors,
+                          save_pars = save_pars( all = TRUE ), refresh = 0 )
+
+  # update Bayesian Poisson model [H0], with intercept only
+  bmodel_H0 <- update( bmodel_H1, formula = seizures ~ 1, refresh = 0 )
+
+  # Bayes Factor, through bridge sampling
+  BF_brms_bridge <- brms::bayes_factor( bmodel_H1, bmodel_H0, log = TRUE )
+
+  print( BF_brms_bridge )
+
+  # output, last row of data
+  output <- data[ nrow( data ), ]
+  output$log_bf <- BF_brms_bridge$bf
+
+  return( output )
+
 }
 
 ################################################################################
@@ -96,11 +127,27 @@ bf3 <- get_bayes_factor( data_set_3 ) #  X log(BF)
 bf4 <- get_bayes_factor( data_set_4 ) #  X log(BF)
 bf5 <- get_bayes_factor( data_set_5 ) #  X log(BF)
 
+# get BFs
+bf1b <- get_bayes_factor2( data_set_1 ) #  X log(BF)
+bf2b <- get_bayes_factor2( data_set_2 ) #  X log(BF)
+bf3b <- get_bayes_factor2( data_set_3 ) #  X log(BF)
+bf4b <- get_bayes_factor2( data_set_4 ) #  X log(BF)
+bf5b <- get_bayes_factor2( data_set_5 ) #  X log(BF)
+
 # combine
 container1 <- rbind( bf1, bf2 )
 container2 <- rbind( bf3, bf4 )
 container3 <- rbind( container1, container2 )
 container <- rbind( container3, bf5 )
+
+# combine
+container1b <- rbind( bf1b, bf2b )
+container2b <- rbind( bf3b, bf4b )
+container3b <- rbind( container1b, container2b )
+containerb <- rbind( container3b, bf5b )
+# the evidence here is actually lower than with the previous model as the effect is much more extreme than expected
+# this might however change if the treatment was less strong, i.e., only rate of 1.5 or 2
+
 
 # print
 print( round( container$log_bf, 1 ) )
